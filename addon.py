635fbc5bde6124e140d0c6bc19bdadd4a97d6e93
Joakim Sandström <joakim.s@webbkontakt.net>
2022-01-07 22:00:39 +0100

    Update addon.py
    Changed ' to " where needed in Dialog text, updated git_url and version too. put the execute of bmilhams script too

--- addon.py
+++ addon.py
@@ -14,14 +14,14 @@ import xbmcaddon
 __plugin__ = "plugin.program.iptv-list_refresh"
 __author__ = "Zjoasan"
 __url__ = "https://github.com/zjoasan/"
-__git_url__ = "https://github.com/zjoasan/"
+__git_url__ = "https://github.com/zjoasan/plugin.program.iptv-list_refresh/"
 __credits__ = "https://github.com/bmillham"
-__version__ = "0.0.3"
+__version__ = "0.0.1"
 
 dialog = xbmcgui.Dialog()
 addon = xbmcaddon.Addon(id='plugin.program.iptv-list_refresh')
-response = dialog.yesno('Reboot Warning', 'This will refresh you local m3u, but will reboot the system when it's done. Continue?', yeslabel='Yes', nolabel='NO!')
+response = dialog.yesno("Reboot Warning", "This will refresh you local m3u, but will reboot the system when it's done. Continue?", yeslabel="Yes", nolabel="NO!")
 if resonse:
-	
+	xbmc.executebuiltin('RunScript(special://home/addons/plugin.program.iptv-list_refresh/resources/lib/iptv-list.py)')
 else:
 	xbmc.executebuiltin('ActivateWindow(10000,return)')
