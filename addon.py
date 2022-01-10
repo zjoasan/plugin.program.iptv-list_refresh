e2e5c76b48b473ab02ef24a42d67875847b7e9a2
Joakim Sandström <joakim.s@webbkontakt.net>
2022-01-10 04:13:11 +0100

    rename kodi ia

--- addon.py
+++ addon.py
@@ -16,11 +16,11 @@ __author__ = "Zjoasan"
 __url__ = "https://github.com/zjoasan/"
 __git_url__ = "https://github.com/zjoasan/plugin.program.iptv-list_refresh/"
 __credits__ = "https://github.com/bmillham"
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 dialog = xbmcgui.Dialog()
 addon = xbmcaddon.Addon(id='plugin.program.iptv-list_refresh')
-response = dialog.yesno("Reboot Warning", "This will refresh you local m3u, but will reboot the system when it's done. Continue?", yeslabel="Yes", nolabel="NO!")
+response = dialog.yesno("Reboot Warning", "This will refresh you local m3u(3+ minutes), then restart IPTVSimple?", yeslabel="Yes", nolabel="NO!")
 if response:
 	xbmc.executebuiltin('RunScript(special://home/addons/plugin.program.iptv-list_refresh/resources/lib/iptv-list.py)')
 else:
--- resources/lib/iptv-list.py
+++ resources/lib/iptv-list.py
@@ -15,7 +15,7 @@ localout = addon.getSetting('entry4')
 
 #from m3u_parser import M3uParser
 from m3u_parser.m3u_parser import M3uParser
-import kodi-ia.kodi-ia
+import kodi_ia.kodi_ia
 
 parser = M3uParser()
 # You could set check_live to True to only grab streams that are tested and working.
@@ -33,8 +33,8 @@ parser.filter_by(key='category', filters=['^Sverige$',
                                      '^Australien$',
                                      '^For Adults$',
                                      '\[VOD\]$'])
-kodi-ia.disable_addon(pvr.iptvsimple)
+kodi_ia.disable_addon(pvr.iptvsimple)
 parser.to_file(localout, format="m3u")
-kodi-ia.change_m3u(localout)
-kodi-ia.enable_addon(pvr.iptvsimple)
+kodi_ia.change_m3u(localout)
+kodi_ia.enable_addon(pvr.iptvsimple)
 
